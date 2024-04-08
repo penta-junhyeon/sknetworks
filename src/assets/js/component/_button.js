8bc170db8b2d24a7dbb15bf2c1f6e80e83bc89c4
choi junhyeon <95choiJH@gmail.com>
2024-04-08 09:59:45 +0900

    폴더 정리

--- src/assets/js/component/_button.js
+++
@@ -1,5 +0,0 @@
-// 가이드(pc,mo) - 헤더 메뉴 활성화/비활성화 기능
-function asideToggleActive() {
-    const layoutGuideList = document.getElementById("guide-list");
-    layoutGuideList.classList.toggle("open-aside");
-}
--- src/assets/js/guide.js
+++ src/assets/js/guide.js
@@ -10,3 +10,9 @@ function headerBackChange() {
         head.classList.remove('back-white')
     });
 }
+
+// 가이드(pc,mo) - 헤더 메뉴 활성화/비활성화 기능
+function asideToggleActive() {
+    const layoutGuideList = document.getElementById("guide-list");
+    layoutGuideList.classList.toggle("open-aside");
+}
