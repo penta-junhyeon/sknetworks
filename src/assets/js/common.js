e7c5d27a9f01db93cf68db9ca8e85a711cb3cf74
95choijh <95choijh@gmail.com>
2024-04-27 21:44:55 +0900

    폰트 교체 및 pc 헤더 작업

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -31,19 +31,42 @@ function frontCommonScroll() {
 }
 
 function header() {
-    
-    const _header = document.getElementById("header")
-    const navi = _header.querySelector('.site-navi')
-    const depth1Item = navi.querySelectorAll('.depth1-item')
-    const depth2Wrap = navi.querySelectorAll('.depth2-wrap')
 
-    // depth1Item.forEach(depth1Item => {
-    //     depth1Item.addEventListener('mouseover', function() {
-    //         depth2Wrap.forEach(depth2Wrap => {
-    //             depth2Wrap.classList.remove('active')
-    //         });
-    //         const curDepth2Wrap = this.querySelector('.depth2-wrap')
-    //         curDepth2Wrap.classList.add('active')
-    //     });
-    // });
+  const _header = document.getElementById("header")
+  const depth1Item = _header.querySelectorAll(".depth1-item")
+
+  if (_header) {
+    // depth2가 8개 이상일 때 고유 스타일 추가
+    depth1Item.forEach(depth1Item => {
+      const depth2List = depth1Item.querySelector(".depth2-list")
+      const depth2Length = depth2List.querySelectorAll(".depth2-item").length
+      if (depth2Length > 7) {
+        depth2List.style.maxWidth = "73.6rem"
+        depth2List.style.flexWrap = "wrap"
+      }
+    });
+
+    // 언어 셀렉트버튼 기능
+    const lang = document.getElementById("lang")
+    const selected = lang.querySelector(".selected")
+    const optionArea = lang.querySelector(".option-area")
+
+    selected.addEventListener("click", function () {
+      optionArea.classList.contains("active") ? optionArea.classList.remove("active") : optionArea.classList.add("active")
+    })
+  }
+}
+
+function select() {
+  const select = document.querySelectorAll(".field.select")
+
+  select.forEach(select => {
+    const selected = select.querySelector(".selected")
+    const optionArea = select.querySelector(".option-area")
+    const optionList = optionArea.querySelectorAll(".option-list")
+
+    selected.addEventListener("click", function () {
+      optionArea.classList.contains("active") ? optionArea.classList.remove("active") : optionArea.classList.add("active")
+    })
+  });
 }
